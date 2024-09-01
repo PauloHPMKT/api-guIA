053776c84d0d57323216e54313f29d4353e729f5
Paulo Sergio <pauloserg861@gmail.com>
2024-09-01 16:00:13 -0300

    refactor: set envfile to reflect port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = new ConfigService();
+  const port = configService.get<string>('APP_PORT');
+
+  await app
+    .listen(port)
+    .then(() =>
+      logger.log(`[GUIA-API] Server running on http://localhost:${port}`),
+    );
 }
 bootstrap();
