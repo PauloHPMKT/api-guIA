0198c91a2e1a91d10871c09f2325deb810d1b555
Paulo Sergio <pauloserg861@gmail.com>
2024-09-01 13:04:53 -0300

    install commitlinter

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: [
+    'build',
+    'chore',
+    'ci',
+    'docs',
+    'feat',
+    'fix',
+    'perf',
+    'refactor',
+    'revert',
+    'style',
+    'test',
+  ],
+};
